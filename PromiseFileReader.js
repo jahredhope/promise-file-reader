54a71563568b1d908451712f74934c29b6ac38be
Anton Gilgur <agilgur5@gmail.com>
2016-10-01 19:25:14 -0400

    (refactor): simplify logic with a single readAs helper function
    - All specific functions now simply call readAs with the type
      of data they would like to read as

--- PromiseFileReader.js
+++ PromiseFileReader.js
@@ -1,4 +1,4 @@
-function readAsDataURL(file) {
+function readAs (file, as) {
   if (!(file instanceof Blob)) {
     throw new TypeError('Must be a File or Blob')
   }
@@ -6,32 +6,20 @@ function readAsDataURL(file) {
     const reader = new FileReader()
     reader.onload = e => resolve(e.target.result)
     reader.onerror = e => reject(`Error reading ${file.name}: ${e.target.result}`)
-    reader.readAsDataURL(file)
-  });
+    reader['readAs' + as](file)
+  })
 }
 
-function readAsText(file) {
-  if (!(file instanceof Blob)) {
-    throw new TypeError('Must be a File or Blob')
-  }
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onload = e => resolve(e.target.result)
-    reader.onerror = e => reject(`Error reading ${file.name}: ${e.target.result}`)
-    reader.readAsText(file)
-  });
+function readAsDataURL (file) {
+  return readAs(file, 'DataURL')
 }
 
-function readAsArrayBuffer(file) {
-  if (!(file instanceof Blob)) {
-    throw new TypeError('Must be a File or Blob')
-  }
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onload = e => resolve(e.target.result)
-    reader.onerror = e => reject(`Error reading ${file.name}: ${e.target.result}`)
-    reader.readAsArrayBuffer(file)
-  });
+function readAsText (file) {
+  return readAs(file, 'Text')
+}
+
+function readAsArrayBuffer (file) {
+  return readAs(file, 'ArrayBuffer')
 }
 
 module.exports = { readAsDataURL, readAsText, readAsArrayBuffer }
