9ff9d94daa96eff8e7ffab06d3d8f35e8da29646
rajsite <rajsite@users.noreply.github.com>
2018-02-16 00:57:47 -0600

    Change from const to var

--- PromiseFileReader.js
+++ PromiseFileReader.js
@@ -3,7 +3,7 @@ function readAs (file, as) {
     throw new TypeError('Must be a File or Blob')
   }
   return new Promise(function(resolve, reject) {
-    const reader = new FileReader()
+    var reader = new FileReader()
     reader.onload = function(e) { resolve(e.target.result) }
     reader.onerror = function(e) { reject(new Error('Error reading' + file.name + ': ' + e.target.result)) }
     reader['readAs' + as](file)
